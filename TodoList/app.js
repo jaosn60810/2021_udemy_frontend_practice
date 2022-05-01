d4f15d8a7310815e488b374e92df76a35c5deed7
jason60810 <jason60810@gmail.com>
2022-05-02 00:34:44 +0800

    297

--- TodoList/app.js
+++ TodoList/app.js
@@ -128,3 +128,50 @@ if (myList) {
     section.appendChild(todo);
   });
 }
+
+function mergeTime(arr1, arr2) {
+  const result = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (Number(arr1[i].todoMonth) > Number(arr2[j].todoMonth)) {
+      result.push(arr2[j]);
+      j++;
+    } else if (Number(arr1[i].todoMonth) < Number(arr2[j].todoMonth)) {
+      result.push(arr1[i]);
+      i++;
+    } else if (Number(arr1[i].todoMonth) === Number(arr2[j].todoMonth)) {
+      if (Number(arr1[i].todoDate) > Number(arr2[j].todoDate)) {
+        result.push(arr2[j]);
+        j++;
+      } else {
+        result.push(arr1[i]);
+        i++;
+      }
+    }
+  }
+
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+
+  return result;
+}
+
+function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  } else {
+    const middle = Math.floor(arr.length / 2);
+    const right = arr.slice(0, middle);
+    const left = arr.slice(middle, arr.length);
+    return mergeTime(mergeSort(right), mergeSort(left));
+  }
+}
