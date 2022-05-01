de38c11057ef3eb4376c892aea388341829599f3
jason60810 <jason60810@gmail.com>
2022-05-02 00:43:27 +0800

    298

--- TodoList/app.js
+++ TodoList/app.js
@@ -75,58 +75,60 @@ add.addEventListener('click', (e) => {
   section.appendChild(todo);
 });
 
-const myList = localStorage.getItem('list');
-if (myList) {
-  const myListArray = JSON.parse(myList);
+function loadData() {
+  const myList = localStorage.getItem('list');
+  if (myList) {
+    const myListArray = JSON.parse(myList);
 
-  myListArray.forEach((item) => {
-    const todo = document.createElement('div');
-    todo.classList.add('todo');
-    const text = document.createElement('p');
-    text.classList.add('todo-text');
-    text.textContent = item.todoText;
-    const time = document.createElement('p');
-    time.classList.add('todo-time');
-    time.textContent = `${item.todoMonth} / ${item.todoDate}`;
-    todo.appendChild(text);
-    todo.appendChild(time);
+    myListArray.forEach((item) => {
+      const todo = document.createElement('div');
+      todo.classList.add('todo');
+      const text = document.createElement('p');
+      text.classList.add('todo-text');
+      text.textContent = item.todoText;
+      const time = document.createElement('p');
+      time.classList.add('todo-time');
+      time.textContent = `${item.todoMonth} / ${item.todoDate}`;
+      todo.appendChild(text);
+      todo.appendChild(time);
 
-    const completeButton = document.createElement('button');
-    completeButton.classList.add('complete');
-    completeButton.innerHTML = `<i class="fa-solid fa-check"></i>`;
-    completeButton.addEventListener('click', (e) => {
-      const todoItem = e.target.parentElement;
-      todoItem.classList.toggle('done');
-    });
-
-    const trashButton = document.createElement('button');
-    trashButton.classList.add('trash');
-    trashButton.innerHTML = `<i class="fa-solid fa-trash"></i>`;
-    trashButton.addEventListener('click', (e) => {
-      const todoItem = e.target.parentElement;
-      todoItem.addEventListener('animationend', () => {
-        const text = todoItem.children[0].textContent;
-        const myListArray = JSON.parse(localStorage.getItem('list'));
-        myListArray.forEach((item, index) => {
-          if (item.todoText === text) {
-            myListArray.splice(index, 1);
-            localStorage.setItem('list', JSON.stringify(myListArray));
-          }
-        });
-
-        todoItem.remove();
+      const completeButton = document.createElement('button');
+      completeButton.classList.add('complete');
+      completeButton.innerHTML = `<i class="fa-solid fa-check"></i>`;
+      completeButton.addEventListener('click', (e) => {
+        const todoItem = e.target.parentElement;
+        todoItem.classList.toggle('done');
       });
 
-      todoItem.style.animation = 'scaleDown 0.3s forwards';
+      const trashButton = document.createElement('button');
+      trashButton.classList.add('trash');
+      trashButton.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+      trashButton.addEventListener('click', (e) => {
+        const todoItem = e.target.parentElement;
+        todoItem.addEventListener('animationend', () => {
+          const text = todoItem.children[0].textContent;
+          const myListArray = JSON.parse(localStorage.getItem('list'));
+          myListArray.forEach((item, index) => {
+            if (item.todoText === text) {
+              myListArray.splice(index, 1);
+              localStorage.setItem('list', JSON.stringify(myListArray));
+            }
+          });
+
+          todoItem.remove();
+        });
+
+        todoItem.style.animation = 'scaleDown 0.3s forwards';
+      });
+
+      todo.appendChild(completeButton);
+      todo.appendChild(trashButton);
+
+      todo.style.animation = 'scaleUp 0.3s forwards';
+
+      section.appendChild(todo);
     });
-
-    todo.appendChild(completeButton);
-    todo.appendChild(trashButton);
-
-    todo.style.animation = 'scaleUp 0.3s forwards';
-
-    section.appendChild(todo);
-  });
+  }
 }
 
 function mergeTime(arr1, arr2) {
@@ -175,3 +177,18 @@ function mergeSort(arr) {
     return mergeTime(mergeSort(right), mergeSort(left));
   }
 }
+
+loadData();
+
+const sortButton = document.querySelector('.sort');
+sortButton.addEventListener('click', () => {
+  const sortedArray = mergeSort(JSON.parse(localStorage.getItem('list')));
+  localStorage.setItem('list', JSON.stringify(sortedArray));
+
+  const len = section.children.length;
+  for (let i = 0; i < len; i++) {
+    section.children[0].remove();
+  }
+
+  loadData();
+});
